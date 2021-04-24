ca45e687662640af25f1df6553dfbdde8f428ed4
Erik Cervin-Edin <erik@cervined.in>
2021-04-24 13:26:47 +0200

    commit everything

---
+++ Binary.java
@@ -0,0 +1,23 @@
+/**
+ * Base class for all binary operators
+ */
+public abstract class Binary 
+  extends Sexpr
+{
+  protected Sexpr left;
+  protected Sexpr right;
+  
+  public Binary(Sexpr left, Sexpr right) {
+    this.left = left;
+    this.right = right;
+  }
+  
+  /* 
+   * The toString-method should be refined so that parentheses are used only when needed
+   */
+  @Override	
+  public String toString() {
+    return "(" + left + ")" + getName() + "(" + right + ")"; 
+  }
+}
+  
